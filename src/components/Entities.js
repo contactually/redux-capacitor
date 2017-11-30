4f7f86880a9757eeddd31d414b938bcc714b8e5c
Austin Pivarnik <austin@contactually.com>
2017-11-30 12:18:08 -0500

    Make Entities component more testable

--- src/components/Entities.js
+++ src/components/Entities.js
@@ -91,6 +91,79 @@ const handleMissingIdsChange = (newProps, oldProps = {}) => {
 // Declare this here to keep the object reference constant
 const defaultPassedIds = []
 
+const createMapState = (containerKeys) => (initialState, initialProps) => {
+  const selectors = containerKeys.reduce((memo, key) => {
+    const { containerId } = initialProps[key]
+
+    memo[key] = {
+      state: EntitiesModule.selectors.containerState(containerId),
+      items: EntitiesModule.selectors.containerItems(containerId),
+      missingIds: EntitiesModule.selectors.containerMissingIds(containerId)
+    }
+    return memo
+  }, {})
+
+  // Needs to be a function so that redux will use this as the
+  // mapStateToProps function on subsequent renders.
+  return (globalState, props) => containerKeys.reduce((memo, key) => {
+    const containerProps = { ...props[key], type: containers[key].type }
+    if (containers[key].getId) {
+      containerProps.ids = containers[key].getId({ ...props, ...memo }) || defaultPassedIds
+    } else if (props.itemId) {
+      containerProps.ids = props.itemId
+    }
+
+    const state = selectors[key].state(globalState, containerProps)
+    const items = selectors[key].items(globalState, containerProps)
+
+    memo[key] = {
+      items,
+      missingIds: selectors[key].missingIds(globalState, containerProps),
+
+      // Convenience helpers derived from state/items
+      errors: state.get('errors'),
+      item: items.first(),
+      filters: state.get('filters'),
+      isFetched: !!state.get('latestRequest'),
+      isLoading: !!state.get('activeRequests').size,
+      total: state.getIn(['meta', 'total'], 0),
+      meta: state.getIn(['meta'])
+    }
+
+    return memo
+  }, {})
+}
+
+const createMapDispatch = (containerKeys) => (initialDispatch, initialProps) => {
+  const boundActions = containerKeys.reduce((memo, key) => {
+    const defaultPayload = {
+      containerId: initialProps[key].containerId,
+      type: containers[key].type
+    }
+
+    memo[key] = bindActionCreators({
+      fetchAll: EntitiesModule.actions.S.FETCH_ALL.trigger,
+      initialize: EntitiesModule.actions.S.INITIALIZE_CONTAINER.trigger,
+      performAction: EntitiesModule.actions.S.PERFORM_ACTION.trigger,
+      updateFilters: EntitiesModule.actions.S.UPDATE_FILTERS.trigger
+    }, initialDispatch, defaultPayload)
+
+    // For backwards compatibility with item/collection
+    const { performAction } = memo[key]
+    memo[key].performAction =
+      (action, options = {}) => typeof action === 'string'
+        ? performAction({ action, ...options })
+        : performAction({ ...action, ...options })
+    memo[key].updateCollection = ({ tolerance, filters } = {}) => memo[key].updateFilters({ tolerance, filters })
+
+    return memo
+  }, {})
+
+  // Needs to be a function so that redux will use this as the
+  // mapDispatchToProps function on subsequent renders.
+  return () => boundActions
+}
+
 /**
  * [entities description]
  * @param  {Object} containers
@@ -101,78 +174,8 @@ const defaultPassedIds = []
 function entities (containers, sagas = {}, identPrefix) {
   const containerKeys = Object.keys(containers)
 
-  const mapState = (initialState, initialProps) => {
-    const selectors = containerKeys.reduce((memo, key) => {
-      const { containerId } = initialProps[key]
-
-      memo[key] = {
-        state: EntitiesModule.selectors.containerState(containerId),
-        items: EntitiesModule.selectors.containerItems(containerId),
-        missingIds: EntitiesModule.selectors.containerMissingIds(containerId)
-      }
-      return memo
-    }, {})
-
-    // Needs to be a function so that redux will use this as the
-    // mapStateToProps function on subsequent renders.
-    return (globalState, props) => containerKeys.reduce((memo, key) => {
-      const containerProps = { ...props[key], type: containers[key].type }
-      if (containers[key].getId) {
-        containerProps.ids = containers[key].getId({ ...props, ...memo }) || defaultPassedIds
-      } else if (props.itemId) {
-        containerProps.ids = props.itemId
-      }
-
-      const state = selectors[key].state(globalState, containerProps)
-      const items = selectors[key].items(globalState, containerProps)
-
-      memo[key] = {
-        items,
-        missingIds: selectors[key].missingIds(globalState, containerProps),
-
-        // Convenience helpers derived from state/items
-        errors: state.get('errors'),
-        item: items.first(),
-        filters: state.get('filters'),
-        isFetched: !!state.get('latestRequest'),
-        isLoading: !!state.get('activeRequests').size,
-        total: state.getIn(['meta', 'total'], 0),
-        meta: state.getIn(['meta'])
-      }
-
-      return memo
-    }, {})
-  }
-
-  const mapDispatch = (initialDispatch, initialProps) => {
-    const boundActions = containerKeys.reduce((memo, key) => {
-      const defaultPayload = {
-        containerId: initialProps[key].containerId,
-        type: containers[key].type
-      }
-
-      memo[key] = bindActionCreators({
-        fetchAll: EntitiesModule.actions.S.FETCH_ALL.trigger,
-        initialize: EntitiesModule.actions.S.INITIALIZE_CONTAINER.trigger,
-        performAction: EntitiesModule.actions.S.PERFORM_ACTION.trigger,
-        updateFilters: EntitiesModule.actions.S.UPDATE_FILTERS.trigger
-      }, initialDispatch, defaultPayload)
-
-      // For backwards compatibility with item/collection
-      const { performAction } = memo[key]
-      memo[key].performAction =
-        (action, options = {}) => typeof action === 'string'
-          ? performAction({ action, ...options })
-          : performAction({ ...action, ...options })
-      memo[key].updateCollection = ({ tolerance, filters } = {}) => memo[key].updateFilters({ tolerance, filters })
-
-      return memo
-    }, {})
-
-    // Needs to be a function so that redux will use this as the
-    // mapDispatchToProps function on subsequent renders.
-    return () => boundActions
-  }
+  const mapState = createMapState(containerKeys)
+  const mapDispatch = createMapDispatch(containerKeys)
 
   const containerPropsCache = {}
   const mergeProps = (stateProps, dispatchProps, props) => {
@@ -256,6 +259,13 @@ function entities (containers, sagas = {}, identPrefix) {
   }
 }
 
-export { containerPropTypes }
+export {
+  containerPropTypes,
+  initializeContainer,
+  handleFiltersChange,
+  handleMissingIdsChange,
+  createMapState,
+  createMapDispatch
+}
 
 export default entities
